660c42a19e2ee6e6f1aa0a37ab86609c8b58f02c
Spandanbalabantaray <spandanb.bsc23@rvu.edu.in>
2024-02-27 14:11:42 +0530

    test

--- set01/problem03.c
+++ set01/problem03.c
@@ -5,21 +5,19 @@ int add(int a, int b);
 void output(int a, int b, int sum);
 
 int main() {
-    int  num1, num2, result;
-    
+    int num1, num2, result;
+
     num1 = input();
     num2 = input();
-    
-    result = add (num1, num2);
+    result = add(num1, num2);
     output(num1, num2, result);
-
-    return 0;
+     return 0;
 }
 
 int input() {
     int num;
-    printf("enter a number: ");
-    scanf("%d, &num");
+    printf("Enter a number: ");
+    scanf("%d", &num);
     return num;
 }
 
@@ -28,5 +26,5 @@ int add(int a, int b) {
 }
 
 void output(int a, int b, int sum) {
-    printf("sum of %d and %d is %d\n", a, b, sum);
+    printf("Sum of %d and %d is %d\n", a, b, sum);
 }
